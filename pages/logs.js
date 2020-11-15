821da0e9a7b0682743ed407b7d30853233825e1a
Morgan Gallant <morgan@morgangallant.com>
2020-11-15 12:02:47 -0500

    pages/logs: per day logs
    Signed-off-by: Morgan Gallant <morgan@morgangallant.com>

--- pages/logs.js
+++ pages/logs.js
@@ -13,6 +13,8 @@ const LogsKey = "bPnmmZV6tK2WntaeCeLekUOGUSMywx5zQBEdSmSwByeFbrTP1o19fN5pyP4v";
 
 export default function Logs() {
   const [logs, setLogs] = useState(null);
+  const [keys, setKeys] = useState([]);
+  const [current, setCurrent] = useState(-1);
 
   /**
    * On component mount, fetch all the logs.
@@ -37,29 +39,155 @@ export default function Logs() {
     doRequest();
   }, []);
 
+  const getDateKey = (date) => {
+    var d = new Date(date);
+    var m = [
+      "Jan",
+      "Feb",
+      "Mar",
+      "Apr",
+      "May",
+      "Jun",
+      "Jul",
+      "Aug",
+      "Sep",
+      "Oct",
+      "Nov",
+      "Dec",
+    ];
+    return (
+      m[d.getMonth()] +
+      " " +
+      ((d.getDate() < 10 ? "0" : "") + d.getDate()) +
+      " " +
+      d.getFullYear()
+    );
+  };
+
+  const days = new Map();
+  if (logs) {
+    logs.logs.forEach((log) => {
+      const ts = Date.parse(log.timestamp);
+      const content = log.contents;
+      const key = getDateKey(ts);
+      const existing = days.has(key) ? days.get(key) : [];
+      existing.push({ timestamp: ts, content: content });
+      existing.sort((a, b) => a.timestamp - b.timestamp);
+      days.set(key, existing);
+    });
+  }
+
+  if (days.size != 0 && keys.length == 0 && current == -1) {
+    const k = Array.from(days.keys()).reverse();
+    setKeys(k);
+    setCurrent(k.length - 1);
+  }
+
   return (
     <Layout>
       <a href="/">&larr; Back to Homepage</a>
       <p>
         <strong>Morgan's Logs</strong>
       </p>
-      <p>
-        Stole this idea from a <a href="http://farza.party/logs">friend</a> -
-        daily logs to "debug" myself. Will eventually incorporate food intake
-        and sleep.
-      </p>
       {logs ? (
         <div>
+          <div
+            style={{
+              display: "flex",
+              width: "100%",
+              height: "50px",
+            }}
+          >
+            <div
+              style={{
+                height: "100%",
+                display: "flex",
+                flexDirection: "column",
+                alignItems: "center",
+                justifyContent: "center",
+                userSelect: "none",
+              }}
+            >
+              <a
+                style={{
+                  cursor: `${current > 0 ? "pointer" : "not-allowed"}`,
+                  color: `${current > 0 ? "blue" : "black"}`,
+                }}
+                onClick={() => {
+                  if (current > 0) {
+                    setCurrent(current - 1);
+                  }
+                }}
+              >
+                &#8592; Previous Day
+              </a>
+            </div>
+            <div
+              style={{
+                height: "100%",
+                display: "flex",
+                flexGrow: "10",
+                paddingLeft: "15px",
+                paddingRight: "15px",
+                alignItems: "center",
+                justifyContent: "center",
+              }}
+            >
+              <p>
+                <strong>{keys[current]}</strong>
+              </p>
+            </div>
+            <div
+              style={{
+                height: "100%",
+                display: "flex",
+                flexDirection: "column",
+                alignItems: "center",
+                justifyContent: "center",
+                userSelect: "none",
+              }}
+            >
+              <a
+                style={{
+                  cursor: `${
+                    current < keys.length - 1 ? "pointer" : "not-allowed"
+                  }`,
+                  color: `${current < keys.length - 1 ? "blue" : "black"}`,
+                }}
+                onClick={() => {
+                  if (current < keys.length - 1) {
+                    setCurrent(current + 1);
+                  }
+                }}
+              >
+                Next Day &#8594;
+              </a>
+            </div>
+          </div>
           <ul>
-            {logs.logs.map((l, i) => {
-              const date = Date.parse(l.timestamp);
-              const dstr = new Date(date).toLocaleString();
+            {days.get(keys[current]).map((l, i) => {
+              const date = new Date(l.timestamp);
               return (
                 <li key={i}>
-                  ({dstr}) {l.contents}
+                  {date.toLocaleTimeString("en-US", {
+                    timeZone: "America/New_York",
+                  })}
+                  {": "}
+                  {l.content}
                 </li>
               );
             })}
+            {/* {logs.logs.map((l, i) => {
+              const date = Date.parse(l.timestamp);
+              const dstr = new Date(date).toLocaleString("en-US", {
+                timeZone: "America/New_York",
+              });
+              return (
+                <li key={i}>
+                  <strong>{dstr}</strong> &#8594; {l.contents}
+                </li>
+              );
+            })} */}
           </ul>
         </div>
       ) : (
